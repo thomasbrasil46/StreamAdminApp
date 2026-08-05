a4e33f41b9cca41cee84fe69cacc2f52650f7c41
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-04 23:10:38 -0300

    Adicionando classes de base e  context

---
+++ StreamAdmin.platforms/Models/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StreamAdmin.platforms.Models.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
---
+++ StreamAdmin.platforms/Models/Context/MySQLContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace StreamAdmin.platforms.Models.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(){}
+        public MySQLContext(DbContextOptions<MySQLContext> options): base(options){}
+    }
+}
