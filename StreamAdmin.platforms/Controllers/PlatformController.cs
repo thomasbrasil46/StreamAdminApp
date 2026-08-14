3b91d0b0fd727a939c23f38aa97f08986123a16e
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-14 00:00:43 -0300

    Adição do Platform controller com os primeiros endpoints

---
+++ StreamAdmin.platforms/Controllers/PlatformController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreamAdmin.Catalog.Data.ValueObjects;
+using StreamAdmin.Catalog.Repository;
+
+namespace StreamAdmin.Catalog.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private IPlatformRepository _platformRepository;
+
+        public PlatformController(IPlatformRepository platformRepository)
+        {
+            _platformRepository = platformRepository ?? throw new 
+                ArgumentNullException(nameof(platformRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PlatformVO>>> GetAllPlatforms()
+        {
+            var platforms = await _platformRepository.FindAllPlatforms();
+            return Ok(platforms);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlatformVO>> GetPlatformById(long id)
+        {
+            var platform = await _platformRepository.FindById(id);
+            if (platform == null)
+                return NotFound();
+            return Ok(platform);
+        }
+    }
+}
