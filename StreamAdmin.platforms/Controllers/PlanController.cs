bfe4b889e5c8d17f7c9589138ff1c19bfb76b1e7
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-14 14:29:39 -0300

    Adição do PlanController

---
+++ StreamAdmin.platforms/Controllers/PlanController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreamAdmin.Catalog.Data.ValueObjects;
+using StreamAdmin.Catalog.Repository;
+
+namespace StreamAdmin.Catalog.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PlanController : ControllerBase
+    {
+        private IPlanRepository _planrRepository;
+
+        public PlanController(IPlanRepository planrRepository)
+        {
+            _planrRepository = planrRepository ?? throw new
+                ArgumentNullException(nameof(planrRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PlanVO>>> GetAllPlans()
+        {
+            var plans = await _planrRepository.FindAllPlans();
+            return Ok(plans);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlanVO>> GetPlanById(long id)
+        {
+            var plan = await _planrRepository.FindById(id);
+            if (plan == null)
+                return NotFound();
+            return Ok(plan);
+        }
+    }
+}
