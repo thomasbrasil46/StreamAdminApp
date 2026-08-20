20ecadb0235b945e3b66ca7fe375abfb13835296
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-19 23:56:19 -0300

    Definindo estrtura de consumo do microsserviço

---
+++ StreamAdminFront/Models/PlatformModel.cs
@@ -0,0 +1,14 @@
+﻿using StreamAdmin.Catalog.Data.ValueObjects;
+
+namespace StreamAdminFront.Models
+{
+    public class PlatformModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string WebSiteUrl { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public ICollection<PlanVO> Plans { get; set; } = new List<PlanVO>();
+    }
+}
