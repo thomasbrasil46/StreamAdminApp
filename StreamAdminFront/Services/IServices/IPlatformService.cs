125ebd0b359158635822832f88028b58c2e61762
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-20 12:11:32 -0300

    Definindo as operacoes

---
+++ StreamAdminFront/Services/IServices/IPlatformService.cs
@@ -0,0 +1,13 @@
+﻿using StreamAdminFront.Models;
+
+namespace StreamAdminFront.Services.IServices
+{
+    public interface IPlatformService
+    {
+        Task<IEnumerable<PlatformModel>> FindAllPlatforms();
+        Task<PlatformModel?> FindById(long id);
+        Task<PlatformModel> CreatePlatform(PlatformModel platform);
+        Task<PlatformModel> UpdatePlatform(PlatformModel platform);
+        Task<bool> DeletePlatform(long id);
+    }
+}
