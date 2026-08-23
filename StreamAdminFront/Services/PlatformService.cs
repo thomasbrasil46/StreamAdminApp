09529de1075aeb0b046589009fba84dfc92c35c6
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-22 23:59:14 -0300

    Injeção de dependência da classe Platform Service

---
+++ StreamAdminFront/Services/PlatformService.cs
@@ -0,0 +1,48 @@
+﻿using StreamAdminFront.Models;
+using StreamAdminFront.Services.IServices;
+using StreamAdminFront.Utils;
+
+namespace StreamAdminFront.Services
+{
+    public class PlatformService : IPlatformService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath = "api/v1/platform";
+
+        public PlatformService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+        public async Task<IEnumerable<PlatformModel>> FindAllPlatforms()
+        {
+            var response = await _httpClient.GetAsync(BasePath);
+            return await response.ReadContentAs<List<PlatformModel>>();
+        }
+        public async Task<PlatformModel?> FindById(long id)
+        {
+            var response = await _httpClient.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<PlatformModel?>();
+        }
+        public async Task<PlatformModel> CreatePlatform(PlatformModel platform)
+        {
+            var response = await _httpClient.PostAsJson(BasePath, platform);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<PlatformModel>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+        public async Task<PlatformModel> UpdatePlatform(PlatformModel platform)
+        {
+            var response = await _httpClient.PutAsJson($"{BasePath}/{platform.Id}", platform);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<PlatformModel>();
+            else throw new Exception("Something went wrong when calling API");
+        }
+        public async Task<bool> DeletePlatform(long id)
+        {
+            var response = await _httpClient.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else throw new Exception("Something went wrong when calling API");
+        }        
+    }
+}
--- StreamAdminFront/Utils/HttpClientExtensions.cs
+++ StreamAdminFront/Utils/HttpClientExtensions.cs
@@ -1,18 +1,34 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace StreamAdminFront.Utils
 {
     public static class HttpClientExtensions
     {
-        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response) 
-        { 
-            if(!response.IsSuccessStatusCode)
-            
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+
                 throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
 
-                var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        }
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+        }
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
         }
     }
 }
