7100c460193af2d888719c0633652d2138a9baba
thomasbrasil46 <thomasbrasilpb@gmail.com>
2026-08-23 01:03:31 -0300

    Injeção de dependência na program

--- StreamAdminFront/Program.cs
+++ StreamAdminFront/Program.cs
@@ -1,9 +1,13 @@
+using StreamAdminFront.Services;
+using StreamAdminFront.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+builder.Services.AddHttpClient<IPlatformService, PlatformService>(c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:StreamAdmin.Catalog"]));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
